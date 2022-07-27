6c73b82074d8802e7a371d3626a8358e5fe551ac
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2022-07-27 13:26:12 -0400

    "added movie-info-service"

---
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -0,0 +1,24 @@
+package io.javabrains.moviecatalogservice.resource;
+
+import io.javabrains.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogResource {
+
+
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable String userId)
+    {
+        return Collections.singletonList(
+                new CatalogItem("Transformer", "test", 4)
+        );
+    }
+
+}
