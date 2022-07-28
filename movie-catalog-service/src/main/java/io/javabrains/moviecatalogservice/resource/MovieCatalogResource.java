c72bbcf472d4988a9e5f8e4b80ba4b93c72a4dc0
amandeep-verma <39786879+amandeep-verma@users.noreply.github.com>
2022-07-28 12:51:22 -0400

    RestTemplate

--- movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javabrains/moviecatalogservice/resource/MovieCatalogResource.java
@@ -1,24 +1,45 @@
 package io.javabrains.moviecatalogservice.resource;
 
 import io.javabrains.moviecatalogservice.models.CatalogItem;
+import io.javabrains.moviecatalogservice.models.Movie;
+import io.javabrains.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable String userId)
     {
-        return Collections.singletonList(
-                new CatalogItem("Transformer", "test", 4)
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3)
         );
+
+
+        return ratings.stream().map(rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+ rating.getMovieId(), Movie.class);
+                    return new CatalogItem( movie.getName() , "test", rating.getRating());
+                })
+                .collect(Collectors.toList());
+
+//
+//        return Collections.singletonList(
+//                new CatalogItem("Transformer", "test", 4)
+//        );
     }
 
 }
